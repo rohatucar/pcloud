ceb7f2370f7b1110c0f40829b22670f18ec8d8de
rohat <rohatucar@outlook.com>
2024-05-21 17:56:01 +0300

    add GetPubThumbLink

--- public_links.go
+++ public_links.go
@@ -76,3 +76,48 @@ func (c *PCloudClient) GetFolderPubLink(path string, fileID int, isEU bool) (str
 	}
 	return result.Link, nil
 }
+
+// GetPubThumbLink; https://docs.pcloud.com/methods/public_links/getpubthumblink.html
+func (c *PCloudClient) GetPubThumbLink(fileID int, code, size string, isEU bool) (string, error) {
+	values := url.Values{
+		"auth": {*c.Auth},
+	}
+
+	switch {
+	case fileID >= 0:
+		values.Add("fileid", strconv.Itoa(fileID))
+	default:
+		return "", errors.New("bad params")
+	}
+
+	if code != "" {
+		values.Add("code", code)
+	} else {
+		return "", errors.New("bad params")
+	}
+
+	if size != "" {
+		values.Add("size", size)
+	} else {
+		return "", errors.New("bad params")
+	}
+
+	resp, err := c.Client.Get(urlBuilder("getpubthumblink", values, isEU))
+	if err != nil {
+		return "", err
+	}
+	defer resp.Body.Close()
+
+	result := struct {
+		Link   string `json:"link"`
+		Result int    `json:"result"`
+		Error  string `json:"error"`
+	}{}
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return "", err
+	}
+	if result.Result > 0 {
+		return "", errors.New(result.Error)
+	}
+	return result.Link, nil
+}
